e65dcf355eaebade4ae1f5df41bf4e66e79e94bf
ahmadyaryeet <saroyaahmad@gmail.com>
2024-09-14 19:21:06 +0100

    Update ArchersAscent_page.js

--- src/components/Games/ArchersAscent_page.js
+++ src/components/Games/ArchersAscent_page.js
@@ -11,15 +11,21 @@ function ArchersAscent() {
         <p>
           Archer's Ascent was one of the first games I published on the Meta Quest store. 
           It's a zombie survival game where you play as an archer trying to survive as long as possible.
+          You are given special powerups which you can use as the round progresses in addition to health orbs. The power-ups give the arrows special abilities such as an explosion, lighting and many more.
         </p>
         
         <h2>Development Process</h2>
         <p>
-          Creating Archer's Ascent taught me valuable lessons about developing large-scale projects 
-          with multiple interacting components. As my first VR game, it also challenged me to quickly 
-          adapt to new software paradigms specific to virtual reality development.
+          It is incredibly difficult and time consuming to create objects to use in unity. Therefore I imported most of the objects I needed to use, from a variety of sources. However I did create the more unique objects myself in blender such as the bow, arrow, special effects when the arrow interacts with objects.
+          Once I had imported all the relevant objects/elements I thought would be usefull I moved on to world building.
         </p>
+
+        <h3>World Building</h3>
+        <p>As there were few viable options online I used a mixture of youtube tutorials and trial and error, to create my own terrain using unity's in built tools. I ended up creating a deserted army base, located on mars, as the setting for my game.</p>
         
+        <h3>Scripts</h3>
+        I created over 150 + scripts for all the elements to interact with each other correctly, mainly the Player, enemies, round detecter, so forth. In the process I learnt about getting very comfortable with kinematics, unity's physics system, projectile motion involving a large amount of calculus.
+
         <h2>Key Takeaways</h2>
         <ul>
           <li>Managing complex interactions in a VR environment</li>
